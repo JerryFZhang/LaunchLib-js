c2f3120a837483d665d616da5a49b2113f411487
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-02-26 12:50:26 -0500

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -5,14 +5,14 @@ const LaunchJS = new Launch()
 let chai = require('chai');
 let should = chai.should();
 
-it('should return an agency with getAgencyById', function(done) {
-    const agency = LaunchJS.get('getAgencyById', '1' );
+it('should return an agency with getAgencyById', function (done) {
+    const agency = LaunchJS.get('getAgencyById', '1');
     console.log(agency)
     done()
 });
 
-it('should return an agency with getAgencyByAbbr', function(done) {
-    const agency = LaunchJS.get('getAgencyByAbbr', 'NASA' );
+it('should return an agency with getAgencyByAbbr', function (done) {
+    const agency = LaunchJS.get('getAgencyByAbbr', 'NASA');
     console.log(agency)
     done()
 });
