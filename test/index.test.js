f91dac2c606e2b30b901b2f3bbb2b4fc63fd06c6
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-03-02 00:07:18 -0500

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -16,3 +16,39 @@ it('should return an agency with getAgencyByAbbr', function (done) {
     console.log(agency)
     done()
 });
+
+it('should return an agency with getAgenciesByName', function (done) {
+    const agency = LaunchJS.get('getAgenciesByName', '1');
+    console.log(agency)
+    done()
+});
+
+it('should return an agency with getAgenciesByType', function (done) {
+    const agency = LaunchJS.get('getAgenciesByType', 'NASA');
+    console.log(agency)
+    done()
+});
+
+it('should return an agency with getAgenciesByCountryCode', function (done) {
+    const agency = LaunchJS.get('getAgenciesByCountryCode', '1');
+    console.log(agency)
+    done()
+});
+
+it('should return an agency with getAgencyTypeById', function (done) {
+    const agency = LaunchJS.get('getAgencyTypeById', 'NASA');
+    console.log(agency)
+    done()
+});
+it('should return an agency with getEventTypeById', function (done) {
+    const agency = LaunchJS.get('getEventTypeById', '1');
+    console.log(agency)
+    done()
+});
+
+it('should return an agency with getEventTypeByName', function (done) {
+    const agency = LaunchJS.get('getEventTypeByName', 'NASA');
+    console.log(agency)
+    done()
+});
+
